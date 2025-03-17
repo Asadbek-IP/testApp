9841e9c07256f2d32a2893db15d34d1a9ea0ecdb
asadcoder <dasturchi6678@gmail.com>
2025-03-18 01:13:27 +0500

    Initial Commit

---
+++ testApp/testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppApp.swift
+//  testApp
+//
+//  Created by Asadbek Bekchanov on 18/03/25.
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
